ee1dabde791965f0b4a9a49830c2501f2eee70c0
Juan <Bottaro.JuanManuel@hotmail.com>
2025-02-03 17:38:42 -0300

    ScrollAgregado

--- src/components/Carrusel.tsx
+++ src/components/Carrusel.tsx
@@ -23,7 +23,7 @@ export function CarouselCustomNavigation(): JSX.Element {
               }
               className="text-white text-2xl bg-black/50 p-2 rounded-full"
             >
-              ‹
+              ❮
             </button>
             <button
               onClick={() =>
@@ -31,7 +31,7 @@ export function CarouselCustomNavigation(): JSX.Element {
               }
               className="text-white text-2xl bg-black/50 p-2 rounded-full"
             >
-              ›
+              ❯
             </button>
           </div>
           {/* Indicadores (dots) en la parte inferior */}
@@ -50,16 +50,19 @@ export function CarouselCustomNavigation(): JSX.Element {
       )}
     >
       <img
+        key="slide-1"
         src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
         alt="image 1"
         className="h-full w-full object-cover"
       />
       <img
+        key="slide-2"
         src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
         alt="image 2"
         className="h-full w-full object-cover"
       />
       <img
+        key="slide-3"
         src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
         alt="image 3"
         className="h-full w-full object-cover"
