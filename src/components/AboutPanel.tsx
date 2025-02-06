54ce2434204736d0ce2925f42f76278fc5494d36
Juan <Bottaro.JuanManuel@hotmail.com>
2025-02-06 03:40:58 -0300

    nose

--- src/components/AboutPanel.tsx
+++ src/components/AboutPanel.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ButtonGlassVertical from './ButtonGlassVertical';
 import './AboutPanel.css';
 
@@ -7,12 +7,21 @@ interface AboutPanelProps {
 }
 
 const AboutPanel: React.FC<AboutPanelProps> = ({ closePanel }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    setIsOpen(true);
+  }, []);
+
   return (
-    <div className="about-panel">
+    <div className={`about-panel ${isOpen ? 'open' : ''}`}>
       <h2>Sobre Mí</h2>
       <p>Soy un desarrollador apasionado por crear soluciones web innovadoras y efectivas.</p>
       <div className="close-button">
-        <ButtonGlassVertical onClick={closePanel} label="Cerrar"/>
+        <ButtonGlassVertical onClick={() => {
+          setIsOpen(false);
+          setTimeout(closePanel, 300); // Wait for the animation to finish
+        }} label="Cerrar"/>
       </div>
     </div>
   );
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,16 +1,28 @@
 const Skills = () => {
   const skills = [
-    { name: "Blender", level: "basico", img: "https://download.blender.org/branding/community/blender_community_badge_white.svg", url: "https://www.blender.org/" },
+    // Languages
     { name: "C", level: "intermedio", img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg", url: "https://www.cprogramming.com/" },
     { name: "C#", level: "Intermedio", img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/csharp/csharp-original.svg", url: "https://www.w3schools.com/cs/" },
-    { name: "Git", level: "Avanzado", img: "https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg", url: "https://git-scm.com/" },
     { name: "JavaScript", level: "Avanzado", img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg", url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript" },
-    { name: "Linux", level: "Intermedio", img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg", url: "https://www.linux.org/" },
-    { name: "Node.js", level: "basico", img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg", url: "https://nodejs.org" },
+    { name: "TypeScript", level: "intermedio", img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg", url: "https://www.typescriptlang.org/" },
+
     { name: "PHP", level: "intermedio", img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/php/php-original.svg", url: "https://www.php.net" },
     { name: "Python", level: "Intermedio", img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg", url: "https://www.python.org" },
-    { name: "TypeScript", level: "intermedio", img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg", url: "https://www.typescriptlang.org/" },
-    { name: "Unity", level: "intermedio", img: "https://www.vectorlogo.zone/logos/unity3d/unity3d-icon.svg", url: "https://unity.com/" }
+    
+    { name: "HTML", level: "intermedio", img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg", url: "https://developer.mozilla.org/en-US/docs/Web/HTML" },
+    { name: "CSS", level: "avanzado", img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg", url: "https://developer.mozilla.org/en-US/docs/Web/CSS" },
+    
+    // Frameworks
+    { name: "React", level: "basico", img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg", url: "https://reactjs.org/" },
+    { name: "Node.js", level: "basico", img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg", url: "https://nodejs.org" },
+    { name: "Unity", level: "intermedio", img: "https://www.vectorlogo.zone/logos/unity3d/unity3d-icon.svg", url: "https://unity.com/" },
+    { name: "Tailwind", level: "basico", img: "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg", url: "https://tailwindcss.com/" },
+    
+    // Tools
+    { name: "Git", level: "Avanzado", img: "https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg", url: "https://git-scm.com/" },
+    { name: "GitHub", level: "Avanzado", img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/github/github-original.svg", url: "https://github.com/" },
+    { name: "Linux", level: "Intermedio", img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg", url: "https://www.linux.org/" },
+    { name: "Blender", level: "basico", img: "https://download.blender.org/branding/community/blender_community_badge_white.svg", url: "https://www.blender.org/" }
   ];
 
   const getLevelColor = (level: string) => {
