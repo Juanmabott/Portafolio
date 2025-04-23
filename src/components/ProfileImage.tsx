3272ae28a16e7c203735e6484769d4285b905360
Juan <Bottaro.JuanManuel@hotmail.com>
2025-04-23 02:19:05 -0300

    Refactorizo imagen

--- src/components/ProfileImage.tsx
+++ src/components/ProfileImage.tsx
@@ -3,8 +3,10 @@ import './ProfileImage.css';
 
 const ProfileImage: React.FC = () => {
   return (
-    <div className="profile-image">
-      <img src="./img/pfp.jpg" alt="Foto de perfil" />
+    <div className="profile-image-container">
+      <div className="profile-image">
+        <img src="./img/pfp.jpg" alt="Foto de perfil" />
+      </div>
     </div>
   );
 };
